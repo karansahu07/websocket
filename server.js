508c619bec0a0f767370fdc72bff7ca7e6df4860
Karan <karan.ekarigar@gmail.com>
2025-05-16 16:57:34 +0530

    add some code

--- server.js
+++ server.js
@@ -149,6 +149,10 @@ io.on('connection', (socket) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel websocket backend!");
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
